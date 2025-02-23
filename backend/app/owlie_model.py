e662bd8c2db1c44de64593646aab30018ef12398
Kanak Tanwar <kanaktanwarpro@gmail.com>
2025-02-23 15:41:40 +0530

    update llm model to use gpu if available

--- backend/app/owlie_model.py
+++ backend/app/owlie_model.py
@@ -1,3 +1,4 @@
+import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
@@ -6,10 +7,11 @@ class Owlie:
         """
         Initialize Owlie
         """
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.messages = [
             {"role": "owlie", "content": "You are Owlie, a coding assistant to clear users of their doubts."}
         ]
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
+        self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto").to(self.device)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     def add_message(self, role: str, content: str):
@@ -25,12 +27,19 @@ class Owlie:
         self.messages.append({"role": "user", "content": prompt})
 
         text = self.tokenizer.apply_chat_template(self.messages, tokenize=False, add_generation_prompt=True)
-        model_inputs = self.tokenizer([text], return_tensors="pt").to(self.model.device)
+        model_inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
 
-        generated_ids = self.model.generate(**model_inputs, max_new_tokens=512)
+        with torch.no_grad():
+            generated_ids = self.model.generate(**model_inputs, max_new_tokens=512)
+            
         generated_ids = [
             output_ids[len(input_ids) :] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
         ]
 
         response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
         return response
+    
+if __name__ == '__main__':
+    owlie = Owlie("Qwen/Qwen2.5-Coder-0.5B-Instruct")
+    response = owlie.infer("hello")
+    print(response)
